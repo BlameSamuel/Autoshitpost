262611e61adc6eae4cda0cb93a586fcb88ec5126
BlameSamuel <35589937+BlameSamuel@users.noreply.github.com>
2018-01-31 22:16:03 -0800

    Update autoshitpost.js
    edited "Jebaited" to :jebaited:

--- autoshitpost.js
+++ autoshitpost.js
@@ -139,7 +139,7 @@ And that is why Harry Potter should have carried a 1911.`);
       message.channel.send(`I sexually identify as an the sun. Ever since I was a boy I dreamed of slamming hydrogen isotopes into each other to make helium & light and send it throught the galaxy. People say to me that a person being a star is Impossible and I’m fucking retarded but I don’t care, I’m beautiful. I’m having a plastic surgeon inflate me with hydrogen and raise my temperature to over 6000 °C. From now on I want you guys to call me “Sol” and respect my right to give you vitamin D and probably skin cancer. If you can’t accept me you’re a fusionphobe and need to check your celestial privilege. Thank you for being so understanding.`);
       break;
     case "jebaited":
-      message.channel.send(`Jebaited KAPPA OUTDATED Jebaited POGCHAMP OVERRATED Jebaited LONG HAVE WE WAITED Jebaited NOW WE JEBAITED Jebaited`);
+      message.channel.send(`:jebaited: KAPPA OUTDATED :jebaited: POGCHAMP OVERRATED :jebaited: LONG HAVE WE WAITED :jebaited: NOW WE JEBAITED :jebaited:`);
       break;
     case "iwanttodie":
       message.channel.send(`If me 👦🙋 and my waifu 👧👫❤ both drowning 💦💦💧💧🌊🌊 and I can only save one 😭👆😭 there will be no survivors 👎⛔🚫 cause my waifu doesnt exist 🙅💔😤 and I want 2 die anyway 🔫🔪🔌💯💯`);
